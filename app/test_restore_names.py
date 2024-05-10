b985ad399a6439d165c60e562225afe18f3ba00b
Triukhan <tryukhan.d@gmail.com>
2024-05-10 23:30:28 +0300

    triukhan solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -3,21 +3,36 @@ from app.restore_names import restore_names
 from copy import copy
 
 
-@pytest.fixture
-def full_data():
-    return [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy",}]
-
-
-def test_without_first_name(full_data):
-    copy_data = copy(full_data)
-    del copy_data[0]["first_name"]
-    restore_names(copy_data)
-    assert copy_data == full_data
-
-
-def test_with_none_first_name(full_data):
-    copy_data = copy(full_data)
-    copy_data[0]["first_name"] = None
-    restore_names(copy_data)
-    assert copy_data == full_data
+@pytest.mark.parametrize("users, expected_users", [
+    (
+        [
+            {"first_name": None, "last_name": "Holy",
+             "full_name": "Jack Holy"},
+            {"last_name": "Adams", "full_name": "Mike Adams"},
+        ],
+        [
+            {"first_name": "Jack", "last_name": "Holy",
+             "full_name": "Jack Holy"},
+            {"first_name": "Mike", "last_name": "Adams",
+             "full_name": "Mike Adams"},
+        ]
+    ),
+    (
+        [
+            {"first_name": "Jack", "last_name": "Holy",
+             "full_name": "Jack Holy"},
+            {"first_name": "Mike", "last_name": "Adams",
+             "full_name": "Mike Adams"},
+        ],
+        [
+            {"first_name": "Jack", "last_name": "Holy",
+             "full_name": "Jack Holy"},
+            {"first_name": "Mike", "last_name": "Adams",
+             "full_name": "Mike Adams"},
+        ]
+    )
+])
+def test_restores_names(users: list, expected_users: list) -> None:
+    restore_names(users)
+    assert users == expected_users
 
